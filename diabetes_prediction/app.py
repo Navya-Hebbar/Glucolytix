4bc93c37cfb058cb1610b53b9663067074fffb50
Navya Hebbar <89196064+Navya-Hebbar@users.noreply.github.com>
2025-06-30 18:21:53 +0530

    Update app.py

--- diabetes_prediction/app.py
+++ diabetes_prediction/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import joblib
+import os
 
 app = Flask(__name__)
 
@@ -82,4 +83,5 @@ def predict_diabetes(user_id):
         return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
